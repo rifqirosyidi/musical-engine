2398ed4cb8384a9cf5d6f00efe6b146a43c1a0e1
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-01-02 23:03:41 +0700

    detail page

--- music/urls.py
+++ music/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<int:album_id>/', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hi Music Home")
+
+
+def detail(request, album_id):
+    return HttpResponse(f'The Album ID is {album_id}')
