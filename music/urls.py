93a0a931547822d982324c12ed268392c2561b2c
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-01-03 23:36:16 +0700

    form handling

--- music/urls.py
+++ music/urls.py
@@ -5,4 +5,5 @@ app_name = 'music'
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:album_id>/', views.detail, name='detail'),
+    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Album
+from .models import Album, Song
 
 
 # Create your views here.
@@ -16,4 +16,20 @@ def detail(request, album_id):
     # album = Album.objects.get(id=album_id)
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        context = {
+            'album': album,
+            'error_message': 'The selected song is invalid'
+        }
+        return render(request, 'music/detail.html', context)
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
 
